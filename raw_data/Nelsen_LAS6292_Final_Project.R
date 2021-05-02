b7eb04b368c97085822efbd4efbc6d0dfdc7b6ab
Emilio M. Bruna <embruna@ufl.edu>
2021-05-02 18:42:36 -0400

    suggested edits to your excellent code

--- raw_data/Nelsen_LAS6292_Final_Project.R
+++ raw_data/Nelsen_LAS6292_Final_Project.R
@@ -7,30 +7,73 @@ library(tidyverse)
 tesseract::tesseract_info()
 
 #create the dataframe that will link filenames with OCR text for easy association
-files <- list.files(path = "./raw_cover_images", pattern = "*.JPG", full.names = T)
+
+files <- list.files(path = "./raw_data/raw_cover_images", pattern = "*.JPG", full.names = T)
 
 #begin OCR process
 raw_data <- sapply(files, tesseract::ocr, engine = "por", simplify=TRUE) %>% 
   bind_rows(.id = "id")
-raw_data <- pivot_longer(raw_data,cols = starts_with("./raw_cover_images"),names_to="files")
+
+
+# when things start with punctuation or or slashes they can be a bit complicated 
+# to deal with. I changed the names of the columns to remove 
+# the `./raw_data/raw_cover_images/`
+
+# create a vector with names of cols
+names_cols<-names(raw_data) 
+# convert it to a tibble 
+names_cols <- as_tibble(names_cols) 
+# remove the `./raw_data/raw_cover_images/`
+names_cols$value <- gsub("./raw_data/raw_cover_images/","",names_cols$value)
+# reassign the names of the columns based on these new ones
+names(raw_data) <- names_cols$value
+
+## Once you do that, you can change your `starts_with`
+
+raw_data <- pivot_longer(raw_data,
+                         cols = starts_with("IMG"),
+                         names_to="files")
+
+# Now how about changing that id to be unique? We can use the numberic portion
+# of the photo
+
+raw_data$id <- raw_data$files
+raw_data$id <- gsub("IMG_","",raw_data$id)
+raw_data$id <- gsub(".JPG","",raw_data$id)
+
 
 #basic cleanup of the OCR-generated text. The text will still need minor edits when adding into Tropy
-dirty_text <- raw_data$value
-dirty_text <- gsub("\n", " ", dirty_text)
-dirty_text <- gsub("AHU ACL CU 015", "AHU_ACL_CU_015", dirty_text)
-dirty_text <- gsub("\\n", " ", dirty_text)
-dirty_text <- gsub(" | ig ", " ", dirty_text)
-dirty_text <- gsub(" |  E nda CTT crer Serem o ", " ", dirty_text)
-dirty_text <- gsub(" im O reina qe E | a 2 —", " ", dirty_text)
-dirty_text <- gsub(" al = ", " ", dirty_text)
-dirty_text <- gsub(" ce a a ", " ", dirty_text)
-dirty_text <- gsub("“o Sd 1103- ", " ", dirty_text)
-dirty_text <- gsub(" TA ", " ", dirty_text)
+
+
+# You don't have to create a new dataframe, you can use the same one and point to 
+# the column you are editing in the gsub statement
+# dirty_text <- raw_data$value
+raw_data$value <- gsub("\n", " ", dirty_text)
+raw_data$value <- gsub("AHU ACL CU 015", "AHU_ACL_CU_015", raw_data$value)
+raw_data$value <- gsub("\\n", " ", raw_data$value)
+raw_data$value <- gsub(" | ig ", " ", raw_data$value)
+raw_data$value <- gsub(" |  E nda CTT crer Serem o ", " ", raw_data$value)
+raw_data$value <- gsub(" im O reina qe E | a 2 —", " ", raw_data$value)
+raw_data$value <- gsub(" al = ", " ", raw_data$value)
+raw_data$value <- gsub(" ce a a ", " ", raw_data$value)
+raw_data$value <- gsub("“o Sd 1103- ", " ", raw_data$value)
+raw_data$value <- gsub(" TA ", " ", raw_data$value)
 
 #creation of final dataframe, trimming unnecessary columns, write file to directory
-raw_data["clean_text"] <- dirty_text
-clean_text <-subset(raw_data, select = -c(1,3))
-write.csv(clean_text,".\\clean_OCR_text.csv", row.names = FALSE)
+# Instead of this next line
+# raw_data["clean_text"] <- dirty_text
+# you can reassign like this:
+clean_text <- raw_data
+
+# how about we rename the columns?
+
+clean_text <- clean_text %>% rename("file"="files",
+                                    "label_text"="value")
+
+# no need to select them...we have them.
+# clean_text <-subset(raw_data, select = -c(1,3))
+
+write.csv(clean_text,".\Clean_Data\clean_OCR_text.csv", row.names = FALSE)
 
 
 
